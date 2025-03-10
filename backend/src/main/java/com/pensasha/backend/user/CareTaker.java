e22e383c19dfcb9dd058775ba27875f636deef2a
Samuel Obunge <samuelobunge@gmail.com>
2025-03-10 20:59:13 +0300

    adding caretaker class model

---
+++ backend/src/main/java/com/pensasha/backend/user/CareTaker.java
@@ -0,0 +1,21 @@
+package com.pensasha.backend.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.*;
+
+@Entity
+@Table(name = "caretakers")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CareTaker extends User{
+    
+    @ManyToOne
+    @JoinColumn(name = "property_id")
+    private Property assignedProperty;
+}
