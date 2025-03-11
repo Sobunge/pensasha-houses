33481d572246176165e0ba139d5286f076ae2e9f
Samuel Obunge <samuelobunge@gmail.com>
2025-03-11 20:51:30 +0300

    importing user service to the user controller

--- backend/src/main/java/com/pensasha/backend/user/controllers/UserController.java
+++ backend/src/main/java/com/pensasha/backend/user/controllers/UserController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pensasha.backend.user.services.UserService;
+
 @RestController
 @RequestMapping(name = "/user")
 public class UserController {
