24592d4c31cbd1d66ad214f1405bf5cb1cec4e3b
Samuel Obunge <samuelobunge@gmail.com>
2025-03-11 20:02:03 +0300

    adding user service to the services folder in user folder

--- backend/src/main/java/com/pensasha/backend/user/services/UserService.java
+++ backend/src/main/java/com/pensasha/backend/user/services/UserService.java
@@ -1,4 +1,4 @@
-package com.pensasha.backend.user;
+package com.pensasha.backend.user.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
