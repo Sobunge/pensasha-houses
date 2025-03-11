c98b4600e376c421da39e374f2509c9d9faf071a
Samuel Obunge <samuelobunge@gmail.com>
2025-03-11 20:00:33 +0300

    Adding the user repository to the repository folder

--- backend/src/main/java/com/pensasha/backend/user/UserService.java
+++ backend/src/main/java/com/pensasha/backend/user/UserService.java
@@ -3,6 +3,8 @@ package com.pensasha.backend.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pensasha.backend.user.repositories.UserRepository;
+
 @Service
 public class UserService {
 
--- backend/src/main/java/com/pensasha/backend/user/repositories/UserRepository.java
+++ backend/src/main/java/com/pensasha/backend/user/repositories/UserRepository.java
@@ -1,4 +1,4 @@
-package com.pensasha.backend.user;
+package com.pensasha.backend.user.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
