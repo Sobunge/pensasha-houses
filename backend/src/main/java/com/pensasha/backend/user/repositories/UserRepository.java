9d45b559fcce3e2027983cb96416260206d17f56
Samuel Obunge <samuelobunge@gmail.com>
2025-03-11 20:31:58 +0300

    Adding implementation for the user service

--- backend/src/main/java/com/pensasha/backend/user/repositories/UserRepository.java
+++ backend/src/main/java/com/pensasha/backend/user/repositories/UserRepository.java
@@ -1,9 +1,14 @@
 package com.pensasha.backend.user.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.pensasha.backend.role.Role;
 import com.pensasha.backend.user.models.User;
 
 public interface UserRepository extends JpaRepository<User, String> {
+
+    List<User> findAllByRole(Role role);
 
 }
--- backend/src/main/java/com/pensasha/backend/user/services/UserService.java
+++ backend/src/main/java/com/pensasha/backend/user/services/UserService.java
@@ -1,8 +1,12 @@
 package com.pensasha.backend.user.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.pensasha.backend.role.Role;
+import com.pensasha.backend.user.models.User;
 import com.pensasha.backend.user.repositories.UserRepository;
 
 @Service
@@ -11,14 +15,34 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    //Adding a new user (Admin)
-    
-    //Editing user details (Admin)
+    // Adding a new user (Admin)
+    public User addingAnAdmin(User user) {
+        return userRepository.save(user);
+    }
 
-    //Deleting a user (Admin)
+    // Editing user details (Admin)
+    public User updateUserDetails(User updatedUser) {
+        return userRepository.save(updatedUser);
+    }
 
-    //Getting a single user (Admin)
+    // Deleting a user (Admin)
+    public void deleteUser(String idNumber) {
+        userRepository.deleteById(idNumber);
+    }
 
-    //Getting all users (Admin)
+    // Getting a single user (Admin)
+    public User gettingUser(String idNumber) {
+        return userRepository.findById(idNumber).get();
+    }
+
+    // Getting all users (Admin)
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // Getting a user by role
+    public List<User> gettingUsersByRole(Role role) {
+        return userRepository.findAllByRole(role);
+    }
 
 }
