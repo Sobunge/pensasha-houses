0b0579c6c431e2f853594eb7efbf112b4c47065c
Samuel Obunge <samuelobunge@gmail.com>
2025-03-11 20:58:40 +0300

    updates

--- backend/src/main/java/com/pensasha/backend/user/controllers/UserController.java
+++ backend/src/main/java/com/pensasha/backend/user/controllers/UserController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pensasha.backend.user.models.User;
 import com.pensasha.backend.user.services.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping(name = "/user")
@@ -12,5 +16,15 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    // Adding a new user (Admin)
+
+    // Editing user details (Admin)
+
+    // Deleting a user (Admin)
+
+    // Getting a single user (Admin)
+
+    // Getting all users (Admin)
 
 }
