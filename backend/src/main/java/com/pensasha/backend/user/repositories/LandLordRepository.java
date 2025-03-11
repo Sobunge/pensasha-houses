dd4d5d7587eaff14e4688d82b0bb3c46ebb4fa20
Samuel Obunge <samuelobunge@gmail.com>
2025-03-11 20:06:13 +0300

    adding the landord repository

---
+++ backend/src/main/java/com/pensasha/backend/user/repositories/LandLordRepository.java
@@ -0,0 +1,9 @@
+package com.pensasha.backend.user.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pensasha.backend.user.models.LandLord;
+
+public interface LandLordRepository extends JpaRepository<LandLord, String>{
+
+}
