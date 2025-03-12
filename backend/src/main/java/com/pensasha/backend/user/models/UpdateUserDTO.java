fb01e38c902b7a96efcb3d39f1e47d029cacd088
Samuel Obunge <samuelobunge@gmail.com>
2025-03-12 09:50:05 +0000

    Adding an updateUserDTO to handle the user profile update

---
+++ backend/src/main/java/com/pensasha/backend/user/models/UpdateUserDTO.java
@@ -0,0 +1,13 @@
+package com.pensasha.backend.user.models;
+
+@Data
+public class UpdateUserDTO {
+
+    private String firstName;
+    private String secondName;
+    private String thirdName;
+    private String idNumber;
+    private String phoneNumber;
+    private Role role;
+
+}
