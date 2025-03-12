86ed4ecc6a4c26819ac06e8621206d502b7f3234
Samuel Obunge <samuelobunge@gmail.com>
2025-03-12 17:51:58 +0300

    Fixing the errors in User repository

--- backend/src/main/java/com/pensasha/backend/user/repositories/UserRepository.java
+++ backend/src/main/java/com/pensasha/backend/user/repositories/UserRepository.java
@@ -10,7 +10,7 @@ import com.pensasha.backend.user.models.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findAllByRole(Role role);
-    voids deleteByIdNumber(String idNumber);
+    void deleteByIdNumber(String idNumber);
     User findByIdNumber(String idNumber);
     Boolean existsByIdNumber(String idNumber);
 
