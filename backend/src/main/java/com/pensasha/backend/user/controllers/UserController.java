ae9a94563ef5d7e9e88af62221dd0eacb5bcbe9f
Samuel Obunge <samuelobunge@gmail.com>
2025-03-12 13:00:46 +0000

    Adding method that gets all users

--- backend/src/main/java/com/pensasha/backend/user/controllers/UserController.java
+++ backend/src/main/java/com/pensasha/backend/user/controllers/UserController.java
@@ -107,5 +107,28 @@ public class UserController {
     }
 
     // Getting all users (Admin)
+    @GetMapping("/all")
+    public ResponseEntity<CollectionModel<User>> getAllUsers(@RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        Page<User> usersPage = userService.getAllUsers(pageable);
+
+        if (usersPage.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+
+        List<EntityModel<User>> userResources = usersPage.getContent().stream()
+                .map(user -> EntityModel.of(user,
+                        linkTo(methodOn(UserController.class).gettingUser(user.getIdNumber())).withSelfRel(),
+                        linkTo(methodOn(UserController.class).getAllUsers(page, size)).withRel("all-users")))
+                .collect(Collectors.toList());
+
+        CollectionModel<EntityModel<User>> response = CollectionModel.of(userResources,
+                linkTo(methodOn(UserController.class).getAllUsers(page, size)).withSelfRel());
+
+        return ResponseEntity.ok(response);
+
+    }
 
 }
