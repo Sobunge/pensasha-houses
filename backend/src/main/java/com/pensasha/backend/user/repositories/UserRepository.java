495b1160fa64ea0caeeaf216d74d9df2b90ab291
Samuel Obunge <samuelobunge@gmail.com>
2025-03-12 18:30:18 +0300

    cleaniing my user repository class

--- backend/src/main/java/com/pensasha/backend/user/repositories/UserRepository.java
+++ backend/src/main/java/com/pensasha/backend/user/repositories/UserRepository.java
@@ -11,8 +11,11 @@ import com.pensasha.backend.user.models.User;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     List<User> findAllByRole(Role role);
+
     void deleteByIdNumber(String idNumber);
+
     Optional<User> findByIdNumber(String idNumber);
+
     Boolean existsByIdNumber(String idNumber);
 
 }
