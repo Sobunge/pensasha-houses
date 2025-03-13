45bdc8ffb3684617b8d5ffab3b7208baf36d43f0
Samuel Obunge <samuelobunge@gmail.com>
2025-03-13 19:30:25 +0300

    cleaning code

--- backend/src/main/java/com/pensasha/backend/user/controllers/UserController.java
+++ backend/src/main/java/com/pensasha/backend/user/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.pensasha.backend.user.controllers;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -15,7 +14,6 @@ import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
