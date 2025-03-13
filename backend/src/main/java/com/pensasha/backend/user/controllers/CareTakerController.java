01e2f2b52265ef4d39c6a2c814b53118d96d3958
Samuel Obunge <samuelobunge@gmail.com>
2025-03-13 20:59:18 +0300

    Adding my caretaker implementation path way

---
+++ backend/src/main/java/com/pensasha/backend/user/controllers/CareTakerController.java
@@ -0,0 +1,26 @@
+package com.pensasha.backend.user.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pensasha.backend.user.services.CareTakerService;
+
+@RestController
+@RequestMapping(path = "/user/caretaker")
+public class CareTakerController {
+
+    @Autowired
+    private CareTakerService careTakerService;
+
+    //Getting all caretaker details
+
+    //Getting caretaker details
+
+    //Deleting caretaker details
+
+    //Adding caretaker details
+
+    //Updating caretaker details
+
+}
