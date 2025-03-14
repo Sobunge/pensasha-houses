48bb4a3559e477ea2a2d7a67f89dc205098dc4a0
Samuel Obunge <samuelobunge@gmail.com>
2025-03-14 12:21:29 +0000

    rearranging my code

--- backend/src/main/java/com/pensasha/backend/user/repositories/LandLordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pensasha.backend.user.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pensasha.backend.user.models.LandLord;
-
-public interface LandLordRepository extends JpaRepository<LandLord, Long>{
-
-}
--- backend/src/main/java/com/pensasha/backend/user/repositories/TenantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.pensasha.backend.user.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.pensasha.backend.user.models.Tenant;
-
-public interface TenantRepository extends JpaRepository<Tenant, Long>{
-
-}
