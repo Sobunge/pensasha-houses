af30b9b431176b46f1cb56176a145affc33a8d54
Samuel Obunge <samuelobunge@gmail.com>
2025-03-15 07:10:21 +0000

    removing input validation from the landlord class

--- backend/src/main/java/com/pensasha/backend/user/models/LandLord.java
+++ backend/src/main/java/com/pensasha/backend/user/models/LandLord.java
@@ -19,10 +19,7 @@ import lombok.Setter;
 @Setter
 public class LandLord extends User {
 
-    @OneToMany(mappedBy = "landLord", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Property> Properties;
-
-    @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "bank_details_id", referencedColumnName = "id")
     private BankDetails BankDetails;
+    
 }
