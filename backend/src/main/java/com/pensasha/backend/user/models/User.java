b68f02371d1e06f4c7ed0537369aed33178026de
Samuel Obunge <samuelobunge@gmail.com>
2025-03-15 06:07:58 +0000

    Removing input validation on user and adding it on the UserDTO

--- backend/src/main/java/com/pensasha/backend/user/models/User.java
+++ backend/src/main/java/com/pensasha/backend/user/models/User.java
@@ -5,50 +5,29 @@ import com.pensasha.backend.role.Role;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
-import org.springframework.validation.annotation.Validated;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Inheritance(strategy = InheritanceType.JOINED) // Allows subclasses to extend this entity
-@Validated
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotBlank(message = "First name is required")
-    @Size(min = 3, max = 20, message = "First name must be between 3 and 20 characters")
     private String firstName;
-
     private String secondName;
-    
-    @NotBlank(message = "Third name is required")
-    @Size(min = 3, max = 20, message = "Third name must be between 3 and 20 characters")
     private String thirdName;
-
-    @Column(unique = true, nullable = false)
-    @NotBlank(message = "National ID is required")
-    @Pattern(regexp = "^[0-9]{7,8}$", message = "National ID must be 7-8 digits")
     private String idNumber;
-
-    @NotBlank(message = "Password is required")
-    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
-
-    @NotBlank(message = "Phone number is required")
-    @Pattern(regexp = "^\\+?[0-9]{10,15}$", message = "Invalid phone number")
-    @Column(unique = true, nullable = false)
     private String phoneNumber;
-
     private String profilePicture;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    @NotNull(message = "Role is required")
     private Role role;
-    
+
 }
