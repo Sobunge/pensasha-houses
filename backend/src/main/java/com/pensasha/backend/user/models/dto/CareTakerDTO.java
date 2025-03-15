a36155c038ef56cabe6ef8575419674e97c62646
Samuel Obunge <samuelobunge@gmail.com>
2025-03-15 07:01:12 +0000

    Adding the caretakerdto

---
+++ backend/src/main/java/com/pensasha/backend/user/models/dto/CareTakerDTO.java
@@ -0,0 +1,10 @@
+package com.pensasha.backend.user.models.dto;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CareTakerDTO extends UserDTO {
+
+    private Property assignedProperty;
+
+}
