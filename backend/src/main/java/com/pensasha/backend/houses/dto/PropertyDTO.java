7eaba7d6f791130dd72ca86ce50212866688fe46
Samuel Obunge <samuelobunge@gmail.com>
2025-03-17 22:45:59 +0300

    Adding missing imports to propertyDTO

--- backend/src/main/java/com/pensasha/backend/houses/dto/PropertyDTO.java
+++ backend/src/main/java/com/pensasha/backend/houses/dto/PropertyDTO.java
@@ -1,5 +1,12 @@
 package com.pensasha.backend.houses.dto;
 
+import java.util.Set;
+
+import com.pensasha.backend.houses.Unit;
+import com.pensasha.backend.user.models.LandLord;
+
+import lombok.*;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
