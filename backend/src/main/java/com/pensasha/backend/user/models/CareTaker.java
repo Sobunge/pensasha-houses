c4d6e8f425521c4a55135bb4bd4ae84a16b129d2
Samuel Obunge <samuelobunge@gmail.com>
2025-03-19 20:43:11 +0300

    Removing unused imports

--- backend/src/main/java/com/pensasha/backend/user/models/CareTaker.java
+++ backend/src/main/java/com/pensasha/backend/user/models/CareTaker.java
@@ -1,12 +1,6 @@
 package com.pensasha.backend.user.models;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 import com.pensasha.backend.houses.Property;
-import com.pensasha.backend.houses.dto.PropertyDTO;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
@@ -25,20 +19,5 @@ public class CareTaker extends User {
     @ManyToOne
     @JoinColumn(name = "property_id")
     private Property assignedProperty;
-
-    private Set<Property> convertToPropertyEntities(Set<PropertyDTO> propertyDTOs) {
-        if (propertyDTOs == null) {
-            return new HashSet<>();
-        }
-
-        return propertyDTOs.stream()
-                .map(dto -> {
-                    Property property = new Property();
-                    property.setLandLord(dto.getLandLord()); // Ensure LandLord exists
-                    property.setUnits(dto.getUnits()); // Ensure Unit is mapped
-                    return property;
-                })
-                .collect(Collectors.toSet());
-    }
 
 }
