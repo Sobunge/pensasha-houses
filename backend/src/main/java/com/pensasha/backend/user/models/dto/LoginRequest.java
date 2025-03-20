7e3b54d9f01a98b2326bdc8f34fbb43a94c95b40
Samuel Obunge <samuelobunge@gmail.com>
2025-03-20 22:19:15 +0300

    Creating a login request

---
+++ backend/src/main/java/com/pensasha/backend/user/models/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package com.pensasha.backend.user.models.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequest {
+
+    private String idNumber;
+    private String password;
+}
