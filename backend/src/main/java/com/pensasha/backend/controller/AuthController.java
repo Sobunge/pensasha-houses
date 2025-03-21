d35f00ac11bf27dff896b1d5dfca1b41dd6d7b3c
Samuel Obunge <samuelobunge@gmail.com>
2025-03-21 09:39:58 +0000

    Updating the auhcontroller

--- backend/src/main/java/com/pensasha/backend/controller/AuthController.java
+++ backend/src/main/java/com/pensasha/backend/controller/AuthController.java
@@ -1,5 +1,22 @@
 package com.pensasha.backend.controller;
 
+@RestController
+@RequestMapping("/api")
 public class AuthController {
 
+    //Register
+    @PostMapping("/auth/register")
+    public ResponseEntity<?> addUser(@Valid @RequestBody UserDTO userDTO, BindingResult result) {}
+
+    //login
+    @PostMapping("/auth/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request){}
+
+    //Logout
+    @PostMapping("/api/logout")
+    public ResponseEntity<String> logout(){
+        
+    }
+
+    //Token refresh
 }
