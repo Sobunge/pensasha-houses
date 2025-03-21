85d72d4cf958b6e410240a8a12b18aadb3770ad0
Samuel Obunge <samuelobunge@gmail.com>
2025-03-21 11:45:28 +0000

    changing the customUserDetailsService to reture the UserPrincipal

--- backend/src/main/java/com/pensasha/backend/security/CustomUserDetailsService.java
+++ backend/src/main/java/com/pensasha/backend/security/CustomUserDetailsService.java
@@ -1,30 +1,26 @@
-package com.pensasha.backend.user;
+package com.pensasha.backend.security;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import com.pensasha.backend.entity.UserEntity;
+import com.pensasha.backend.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import com.pensasha.backend.user.models.User;
-
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String idNumber) throws UsernameNotFoundException {
-        Optional<User> userOptional = userRepository.findByIdNumber(idNumber);
-        User user = userOptional.orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        UserEntity user = userRepository.findByIdNumber(idNumber)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
-        return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getIdNumber()) // Use idNumber as username
-                .password(user.getPassword()) // Use the stored hashed password
-                .roles(user.getRole().name()) // Convert Enum to String Role
-                .build();
+        return new UserPrincipal(user);
     }
 }
