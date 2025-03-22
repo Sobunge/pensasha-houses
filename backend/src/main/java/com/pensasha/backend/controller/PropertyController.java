10fac20001809ad67da456f1ac3949d1689797f1
Samuel Obunge <samuelobunge@gmail.com>
2025-03-22 17:07:44 +0000

    adding the propertycontroller

---
+++ backend/src/main/java/com/pensasha/backend/controller/PropertyController.java
@@ -0,0 +1,11 @@
+package com.pensasha.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ResponseMapping("/api/properties/")
+@RequiredArgsConstructor
+public class PropertyController {
+
+}
