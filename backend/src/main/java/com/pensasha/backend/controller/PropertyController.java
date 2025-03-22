4274bf6e8228f1acc20298d819bae20049208e36
Samuel Obunge <samuelobunge@gmail.com>
2025-03-22 18:36:12 +0000

    Adding a method in the property controller to get all properties owned by landlord

--- backend/src/main/java/com/pensasha/backend/controller/PropertyController.java
+++ backend/src/main/java/com/pensasha/backend/controller/PropertyController.java
@@ -1,5 +1,7 @@
 package com.pensasha.backend.controller;
 
+import javax.swing.text.html.parser.Entity;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -49,34 +51,99 @@ public class PropertyController {
             @PathVariable Long id,
             @Valid @RequestBody PropertyDTO propertyDTO,
             BindingResult bindingResult) {
-    
+
         if (bindingResult.hasErrors()) {
             List<String> errors = bindingResult.getFieldErrors().stream()
                     .map(error -> error.getField() + ": " + error.getDefaultMessage())
                     .collect(Collectors.toList());
-    
+
             return ResponseEntity.badRequest().body(Map.of("errors", errors));
         }
-    
+
         Property updatedProperty = propertyService.updateProperty(id, propertyDTO);
-        
+
         // Use the utility method
         PropertyDTO updatedPropertyDTO = PropertyMapperUtil.mapToDTO(updatedProperty);
-    
+
         EntityModel<PropertyDTO> propertyModel = EntityModel.of(updatedPropertyDTO,
                 linkTo(methodOn(PropertyController.class).updateProperty(id, propertyDTO, bindingResult)).withSelfRel(),
                 linkTo(methodOn(PropertyController.class).getAllProperties()).withRel("all-properties"));
-    
+
         return ResponseEntity.ok(propertyModel);
     }
-    
 
     // Getting a property
+    @GetMapping("/{id}")
+    public ResponseEntity<EntityModel<PropertyDTO>> getProperty(@PathVariable Long id) {
+
+        Property property = propertyService.getProperty(id);
+
+        if (property.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        PropertyDTO propertyDTO = PropertyMapperUtil.mapToDTO(property.get());
+
+        EntityModel<PropertyDTO> responseDTO = EntityModel.of(propertyDTO,
+                linkTo(methodOn(PropertyController.class).getProperty(id)).withSelfRel(),
+                linkTo(methodOn(PropertyController.class).getAllProperties()).withRel("all-properties"));
+
+        return ResponseEntity.ok(responseDTO);
+
+    }
 
     // Getting all properties
+    @GetMapping("/all")
+    public ResponseEntity<CollectionModel<EntityModel<PropertyDTO>>> getAllProperties() {
+
+        List<Property> properties = propertyService.getAllProperties();
+
+        if (properties.isEmpty()) {
+            return ResponseEntity.noContent().build(); // Returns 204 No Content if no properties exist
+        }
+
+        List<EntityModel<PropertyDTO>> propertyDTOs = properties.stream()
+                .map(property -> {
+                    PropertyDTO propertyDTO = PropertyMapperUtil.mapToDTO(property);
+                    return EntityModel.of(propertyDTO,
+                            linkTo(methodOn(PropertyController.class).getProperty(property.getId())).withSelfRel());
+                })
+                .collect(Collectors.toList());
+
+        CollectionModel<EntityModel<PropertyDTO>> responseDTO = CollectionModel.of(propertyDTOs,
+                linkTo(methodOn(PropertyController.class).getAllProperties()).withSelfRel());
+
+        return ResponseEntity.ok(responseDTO);
+    }
 
     // Getting all properties belonging to a landlord
+    @GetMapping("/all/{idNumber}")
+    public ResponseEntity<CollectionModel<EntityModel<PropertyDTO>>> getPropertiesByLandlord(
+            @PathVariable String idNumber) {
+
+        List<Property> properties = propertyService.getPropertiesByLandlord(idNumber);
+
+        if (properties.isEmpty()) {
+            return ResponseEntity.noContent().build(); // 204 No Content if no properties exist for the landlord
+        }
+
+        List<EntityModel<PropertyDTO>> propertyDTOs = properties.stream()
+                .map(property -> {
+                    PropertyDTO propertyDTO = PropertyMapperUtil.mapToDTO(property);
+                    return EntityModel.of(propertyDTO,
+                            linkTo(methodOn(PropertyController.class).getProperty(property.getId())).withSelfRel(),
+                            linkTo(methodOn(PropertyController.class).getAllProperties()).withRel("all-properties"));
+                })
+                .collect(Collectors.toList());
+
+        CollectionModel<EntityModel<PropertyDTO>> responseDTO = CollectionModel.of(propertyDTOs,
+                linkTo(methodOn(PropertyController.class).getPropertiesByLandlord(idNumber)).withSelfRel());
+
+        return ResponseEntity.ok(responseDTO);
+    }
 
     // Deleting a property
+    @DeleteMapping("/{id}")
+    
 
 }
