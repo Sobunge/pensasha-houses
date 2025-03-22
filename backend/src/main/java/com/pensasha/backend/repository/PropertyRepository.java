d359323f20d0179c0e140e176dfe2509f4296671
Samuel Obunge <samuelobunge@gmail.com>
2025-03-22 09:55:54 +0000

    Adding missing imports in property repository

---
+++ backend/src/main/java/com/pensasha/backend/repository/PropertyRepository.java
@@ -0,0 +1,9 @@
+package com.pensasha.backend.repository;
+
+import com.pensasha.backend.entity.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface PropertyRepository extends JpaRepository<Property, Long>{
+
+}
