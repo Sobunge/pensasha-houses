d5ea4900e852407e36b932a61c8319aff3607033
Samuel Obunge <samuelobunge@gmail.com>
2025-03-22 09:59:21 +0000

    Adding my property workflow

---
+++ backend/src/main/java/com/pensasha/backend/service/PropertyService.java
@@ -0,0 +1,24 @@
+package com.pensasha.backend.service;
+
+import com.pensasha.backend.repository.PropertyRepository;
+
+@Service
+@RequiredArgConstructor
+public class PropertyService {
+
+    private final PropertyRepository propertyRepository;
+
+    // Adding a property
+
+    // Updating property details
+
+    // Getting one property
+
+    // Geting all properties
+
+    // Deleting a property
+
+    // Getting a property belonging to a landlord (Owner)
+
+
+}
