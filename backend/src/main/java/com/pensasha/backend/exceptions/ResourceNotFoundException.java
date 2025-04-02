5e1ebd58b782d022ce342ffc966798a7f1aa1313
Samuel Obunge <samuelobunge@gmail.com>
2025-04-02 06:10:43 +0000

    Added ResourceNotFoundException

---
+++ backend/src/main/java/com/pensasha/backend/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.pensasha.backend.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
