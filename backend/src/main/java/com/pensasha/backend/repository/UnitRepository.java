54caa912896588e945b153f274f2227b0c71b960
Samuel Obunge <samuelobunge@gmail.com>
2025-04-12 10:27:57 +0000

    Creating a unit repository

---
+++ backend/src/main/java/com/pensasha/backend/repository/UnitRepository.java
@@ -0,0 +1,8 @@
+package com.pensasha.backend.repository;
+
+import com.pensasha.backend.entity.Unit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UnitRepository extends JpaRepository<Unit, Long>{
+
+}
