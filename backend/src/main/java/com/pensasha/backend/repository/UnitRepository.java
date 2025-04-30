031dc39aa6703dd3d56f9d30d0de7650daa97471
Samuel Obunge <samuelobunge@gmail.com>
2025-04-30 10:06:21 +0000

    Adding Unit Repository

--- backend/src/main/java/com/pensasha/backend/repository/UnitRepository.java
+++ backend/src/main/java/com/pensasha/backend/repository/UnitRepository.java
@@ -1,8 +1,13 @@
 package com.pensasha.backend.repository;
 
-import com.pensasha.backend.entity.Unit;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UnitRepository extends JpaRepository<Unit, Long>{
+import com.pensasha.backend.entity.Unit;
 
+public interface UnitRepository extends JpaRepository<Unit, Long> {
+
+    Page<Unit> findByIsOccupied(boolean isOccupied, Pageable pageable);
 }
+
