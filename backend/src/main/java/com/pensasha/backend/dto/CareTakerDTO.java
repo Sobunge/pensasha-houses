ee28b2d0eddf02bd97c4165f863f4d8a6f420018
Samuel Obunge <samuelobunge@gmail.com>
2025-04-30 11:17:03 +0000

    Adding comments to the Caretaker DTO Class

--- backend/src/main/java/com/pensasha/backend/dto/CareTakerDTO.java
+++ backend/src/main/java/com/pensasha/backend/dto/CareTakerDTO.java
@@ -4,12 +4,16 @@ import com.pensasha.backend.entity.Property;
 
 import lombok.*;
 
+// Lombok annotation to generate getter methods automatically for all fields in the class
 @Getter
+// Lombok annotation to generate setter methods automatically for all fields in the class
 @Setter
+// Lombok annotation to generate a no-argument constructor automatically
 @NoArgsConstructor
+// Lombok annotation to generate an all-arguments constructor automatically
 @AllArgsConstructor
 public class CareTakerDTO extends UserDTO {
 
+    // A reference to the Property object that is assigned to the caretaker
     private Property assignedProperty;
-
 }
