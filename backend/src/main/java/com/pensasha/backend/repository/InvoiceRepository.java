e05bdebfabb988f96043ab72102c1e350c11fc85
Sobunge <samuelobunge@gmail.com>
2025-05-02 20:44:45 +0300

    adding methods to the invoice repository

--- backend/src/main/java/com/pensasha/backend/repository/InvoiceRepository.java
+++ backend/src/main/java/com/pensasha/backend/repository/InvoiceRepository.java
@@ -1,9 +1,15 @@
 package com.pensasha.backend.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pensasha.backend.entity.Invoice;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, String>{
+
+    List<Invoice> findByInvoiceDateMonth(int month);
+
+    List<Invoice> findByTenantId(Long tenantId);
 
 }
