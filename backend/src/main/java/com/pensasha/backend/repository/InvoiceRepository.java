122612bb6788ccea220f299cf74af522457c496d
Samuel Obunge <samuelobunge@gmail.com>
2025-05-02 10:29:28 +0000

    creating invoice repository

---
+++ backend/src/main/java/com/pensasha/backend/repository/InvoiceRepository.java
@@ -0,0 +1,9 @@
+package com.pensasha.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pensasha.backend.entity.Invoice;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, Long>{
+
+}
