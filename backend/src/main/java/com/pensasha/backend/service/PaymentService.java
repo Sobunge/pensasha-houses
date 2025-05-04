3e341ec3e6271f27ddeb1a825050564aa3bf53cd
Sobunge <samuelobunge@gmail.com>
2025-05-04 20:25:21 +0300

    Adding a method to add assignPaymentToInvoice method

--- backend/src/main/java/com/pensasha/backend/service/PaymentService.java
+++ backend/src/main/java/com/pensasha/backend/service/PaymentService.java
@@ -3,35 +3,60 @@ package com.pensasha.backend.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.pensasha.backend.entity.Invoice;
 import com.pensasha.backend.entity.Payment;
+import com.pensasha.backend.entity.Tenant;
+import com.pensasha.backend.exceptions.ResourceNotFoundException;
+import com.pensasha.backend.repository.InvoiceRepository;
 import com.pensasha.backend.repository.PaymentRepository;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class PaymentService {
 
-    @Autowired
-    private PaymentRepository paymentRepository;
+    private final PaymentRepository paymentRepository;
+    private final InvoiceRepository invoiceRepository;
 
-    //Adding payment
-    public Payment addingPayment(Payment payment){
+    // Adding payment
+    public Payment addingPayment(Payment payment) {
         return paymentRepository.save(payment);
     }
 
-    //Updating payment details
-    public Payment updatingPaymentDetails(Payment payment){
+    // Updating payment details
+    public Payment updatingPaymentDetails(Payment payment) {
         return paymentRepository.save(payment);
     }
 
-    //Getting payment by id
-    public Optional<Payment> getPaymentById(Long id){
+    // Getting payment by id
+    public Optional<Payment> getPaymentById(Long id) {
         return paymentRepository.findById(id);
     }
 
-    //Getting all payments
+    // Getting all payments
+    public Page<Payment> getAllPayments(Pageable pageable) {
+        return paymentRepository.findAll(pageable);
+    }
 
-    //Deleting payment
+    // Deleting payment
+    public void detelePayment(Long id) {
+        paymentRepository.deleteById(id);
+    }
 
-    //Assigning an invoice to a payment
+    // Assigning an invoice to a payment
+    public Payment assignPaymentToInvoice(String invoiceId, Long paymentId) {
+        Payment payment = paymentRepository.findById(paymentId)
+                .orElseThrow(() -> new ResourceNotFoundException("Payment with id: " + paymentId + " not found"));
+
+        Invoice invoice = invoiceRepository.findById(invoiceId)
+                .orElseThrow(() -> new ResourceNotFoundException("Invoice with id: " + invoiceId + " not found"));
+
+        payment.setInvoice(invoice);
+        return paymentRepository.save(payment);
+    }
 }
