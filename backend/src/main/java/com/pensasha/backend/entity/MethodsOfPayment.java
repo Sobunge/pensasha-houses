2f11d4a5dc50ef4a4fefd9f141d9c4521c01431b
Sobunge <samuelobunge@gmail.com>
2025-05-04 13:44:31 +0300

    Adding an enum Methods of payment

---
+++ backend/src/main/java/com/pensasha/backend/entity/MethodsOfPayment.java
@@ -0,0 +1,20 @@
+package com.pensasha.backend.entity;
+
+/**
+ * Enum representing the available methods of payment 
+ * in the Pensasha backend system.
+ *
+ * The supported payment methods are:
+ * - MPESA: Mobile money transfer service.
+ * - BANK: Payment through a bank account.
+ * - CASH: Physical cash payment.
+ *
+ * This enum can be used in transactions, invoices, 
+ * or any financial records to indicate the payment method used.
+ */
+public enum MethodsOfPayment {
+
+    MPESA, // Mobile money transfer via MPESA
+    BANK,  // Payment made through bank transfer or deposit
+    CASH   // Physical cash payment
+}
