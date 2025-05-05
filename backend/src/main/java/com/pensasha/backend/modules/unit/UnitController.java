98b13fe0f35ba9fe256f30006e5c23c0e8f8a166
Samuel Obunge <samuelobunge@gmail.com>
2025-05-05 11:52:35 +0000

    update

--- backend/src/main/java/com/pensasha/backend/modules/unit/UnitController.java
+++ backend/src/main/java/com/pensasha/backend/modules/unit/UnitController.java
@@ -1,12 +1,11 @@
 package com.pensasha.backend.modules.unit;
 
-import com.pensasha.backend.exceptions.ResourceNotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -16,17 +15,10 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("/api/units")
 @Slf4j
+@AllArgsConstructor
 public class UnitController {
 
     private final UnitService unitService;
-
-    /**
-     * Constructor-based dependency injection for UnitService.
-     */
-    @Autowired
-    public UnitController(UnitService unitService) {
-        this.unitService = unitService;
-    }
 
     /**
      * Adds a new unit.
