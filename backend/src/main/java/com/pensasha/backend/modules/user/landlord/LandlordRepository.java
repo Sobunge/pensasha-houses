5eb4f655333b128b8527e5061f5ef3e922eb4b4f
Samuel Obunge <samuelobunge@gmail.com>
2025-05-10 16:09:31 +0000

    Adding findByIdNumber method

--- backend/src/main/java/com/pensasha/backend/modules/user/landlord/LandlordRepository.java
+++ backend/src/main/java/com/pensasha/backend/modules/user/landlord/LandlordRepository.java
@@ -1,7 +1,10 @@
 package com.pensasha.backend.modules.user.landlord;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LandlordRepository extends JpaRepository<Long, LandLord>{
 
+    Optional<LandLord> findByIdNumber(String idNumber); 
 }
