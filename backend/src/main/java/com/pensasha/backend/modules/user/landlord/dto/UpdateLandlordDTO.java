100a3c9b92a36be78c8399f6e82b43f3b6c1b5f0
Samuel Obunge <samuelobunge@gmail.com>
2025-05-10 16:06:16 +0000

    Updating update landlord dto

--- backend/src/main/java/com/pensasha/backend/modules/user/landlord/dto/UpdateLandlordDTO.java
+++ backend/src/main/java/com/pensasha/backend/modules/user/landlord/dto/UpdateLandlordDTO.java
@@ -1,7 +1,33 @@
 package com.pensasha.backend.modules.user.landlord.dto;
 
+import java.util.Set;
+
+import org.springframework.validation.annotation.Validated;
+
 import com.pensasha.backend.modules.user.dto.UpdateUserDTO;
 
-public class UpdateLandlordDTO extends UpdateUserDTO{
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
+/**
+ * DTO class for updating landlord details, extending UpdateUserDTO.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Validated
+@EqualsAndHashCode(callSuper = true)
+public class UpdateLandlordDTO extends UpdateUserDTO {
+
+    /**
+     * Set of property IDs owned by this landlord.
+     */
+    private Set<Long> propertyIds;
+
+    /**
+     * Bank details associated with this landlord.
+     */
+    private BankDetailsDTO bankDetails;
 }
