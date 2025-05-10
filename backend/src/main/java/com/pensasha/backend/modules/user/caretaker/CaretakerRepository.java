1868123b8834bb93fc1aa85dde22842ed4638615
Samuel Obunge <samuelobunge@gmail.com>
2025-05-10 14:20:55 +0000

    Adding Caretaker repository

---
+++ backend/src/main/java/com/pensasha/backend/modules/user/caretaker/CaretakerRepository.java
@@ -0,0 +1,7 @@
+package com.pensasha.backend.modules.user.caretaker;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CaretakerRepository extends JpaRepository<Long, CareTaker>{
+
+}
