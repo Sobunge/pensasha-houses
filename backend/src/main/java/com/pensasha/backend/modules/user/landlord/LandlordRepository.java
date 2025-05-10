389a99d76a9b96fab4b991bf3915f3692ed2078c
Samuel Obunge <samuelobunge@gmail.com>
2025-05-10 14:44:40 +0000

    Add landlord repository

---
+++ backend/src/main/java/com/pensasha/backend/modules/user/landlord/LandlordRepository.java
@@ -0,0 +1,7 @@
+package com.pensasha.backend.modules.user.landlord;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LandlordRepository extends JpaRepository<Long, LandLord>{
+
+}
