28415e24ffc7dba239aeae84e4d2e44e9a4b33a1
Samuel Obunge <samuelobunge@gmail.com>
2025-05-13 09:12:23 +0000

    update unit repository

--- backend/src/main/java/com/pensasha/backend/modules/unit/UnitRepository.java
+++ backend/src/main/java/com/pensasha/backend/modules/unit/UnitRepository.java
@@ -1,7 +1,5 @@
 package com.pensasha.backend.modules.unit;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -10,12 +8,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
  */
 public interface UnitRepository extends JpaRepository<Unit, Long> {
 
-    /**
-     * Retrieves a page of units that match the specified occupied status.
-     * 
-     * @param isOccupied The occupancy status to filter units by (true for occupied, false for vacant).
-     * @param pageable Pagination information (e.g., page number, page size).
-     * @return A Page of units that are either occupied or vacant based on the provided status.
-     */
-    Page<Unit> findByIsOccupied(boolean isOccupied, Pageable pageable);
 }
