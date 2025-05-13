611b806982016a0922dab5369015fe4f2dbf1c4c
Sobunge <samuelobunge@gmail.com>
2025-05-13 20:27:30 +0300

    Updating caretaker repository

--- backend/src/main/java/com/pensasha/backend/modules/user/caretaker/CaretakerRepository.java
+++ backend/src/main/java/com/pensasha/backend/modules/user/caretaker/CaretakerRepository.java
@@ -2,6 +2,6 @@ package com.pensasha.backend.modules.user.caretaker;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CaretakerRepository extends JpaRepository<Long, CareTaker>{
+public interface CaretakerRepository extends JpaRepository<CareTaker, Long>{
 
 }
