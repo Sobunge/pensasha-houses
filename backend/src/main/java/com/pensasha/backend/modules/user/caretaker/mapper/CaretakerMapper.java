608f1d1c47698f1d974413e54a02dacd14f32877
Samuel Obunge <samuelobunge@gmail.com>
2025-05-14 07:07:40 +0000

    updating the caretakerMapper class

--- backend/src/main/java/com/pensasha/backend/modules/user/caretaker/mapper/CaretakerMapper.java
+++ backend/src/main/java/com/pensasha/backend/modules/user/caretaker/mapper/CaretakerMapper.java
@@ -1,12 +1,35 @@
 package com.pensasha.backend.modules.user.caretaker.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
+import com.pensasha.backend.modules.property.Property;
 import com.pensasha.backend.modules.user.caretaker.Caretaker;
+import com.pensasha.backend.modules.user.caretaker.dto.CaretakerDTO;
 
 @Mapper(componentModel = "spring")
 public interface CaretakerMapper {
 
-    Caretaker toDTO (Caretaker caretaker);
+    @Mapping(source = "assignedProperty.id", target = "propertyId")
+    CaretakerDTO toDTO(Caretaker caretaker);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "enabled", ignore = true)
+    @Mapping(target = "locked", ignore = true)
+    @Mapping(target = "accountExpirationDate", ignore = true)
+    @Mapping(target = "passwordExpirationDate", ignore = true)
+    @Mapping(source = "propertyId", target = "assignedProperty", qualifiedByName = "mapPropertyIdToProperty")
+    Caretaker toEntity(CaretakerDTO caretakerDTO);
+
+    @Named("mapPropertyIdToProperty")
+    default Property mapPropertyIdToProperty(Long propertyId) {
+        if (propertyId == null) {
+            return null;
+        }
+        Property property = new Property();
+        property.setId(propertyId);
+        return property;
+    }
 
 }
