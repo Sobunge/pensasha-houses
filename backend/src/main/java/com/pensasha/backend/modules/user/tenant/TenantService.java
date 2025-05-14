5a9170591d421094c76f184ec07400656a10cb33
Samuel Obunge <samuelobunge@gmail.com>
2025-05-14 13:13:05 +0000

    creating a tenant service

---
+++ backend/src/main/java/com/pensasha/backend/modules/user/tenant/TenantService.java
@@ -0,0 +1,20 @@
+package com.pensasha.backend.modules.user.tenant;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TenantService {
+
+    // Update rental units to a tenant
+
+    //update lease linked to a tenant
+
+    // update emergencyContact linked to a tenant
+
+    // Get a tenant details
+
+    // Getting all tenants using pageable
+
+    // delete a tenant
+
+}
