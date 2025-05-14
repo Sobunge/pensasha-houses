4aba138a458969ef46874544c430658752fa97ae
Samuel Obunge <samuelobunge@gmail.com>
2025-05-14 06:35:52 +0000

    Update user mapper class

--- backend/src/main/java/com/pensasha/backend/modules/user/mapper/UserMapper.java
+++ backend/src/main/java/com/pensasha/backend/modules/user/mapper/UserMapper.java
@@ -1,13 +1,21 @@
 package com.pensasha.backend.modules.user.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.pensasha.backend.modules.user.User;
-import com.pensasha.backend.modules.user.dto.CreateUserDTO;
+import com.pensasha.backend.modules.user.dto.GetUserDTO;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    
-    CreateUserDTO toDTO (User user);
-    User toEntity (CreateUserDTO userDTO);
+
+    GetUserDTO toDTO(User user);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "accountExpirationDate", ignore = true)
+    @Mapping(target = "passwordExpirationDate", ignore = true)
+    @Mapping(target = "enabled", ignore = true)
+    @Mapping(target = "locked", ignore = true)
+    User toEntity(GetUserDTO userDTO);
 }
