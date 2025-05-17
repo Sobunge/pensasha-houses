d6fe330a49dd7a4889b457b984999879d41e0e26
Samuel Obunge <samuelobunge@gmail.com>
2025-05-17 16:22:44 +0000

    Update the leaseMapper interface

--- backend/src/main/java/com/pensasha/backend/modules/lease/mapper/LeaseMapper.java
+++ backend/src/main/java/com/pensasha/backend/modules/lease/mapper/LeaseMapper.java
@@ -1,12 +1,40 @@
 package com.pensasha.backend.modules.lease.mapper;
 
-import org.mapstruct.Mapper;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import com.pensasha.backend.modules.invoice.Invoice;
 import com.pensasha.backend.modules.lease.Lease;
 import com.pensasha.backend.modules.lease.dto.LeaseDTO;
 
 @Mapper(componentModel = "spring")
 public interface LeaseMapper {
 
+    @Mapping(target = "tenant", expression = "java(tenant)")
+    @Mapping(target = "unit", expression = "java(unit)")
+    @Mapping(target = "invoices", ignore = true)
     Lease toEntity(LeaseDTO leaseDTO);
+
+       @Mappings({
+        @Mapping(source = "tenant.id", target = "tenantId"),
+        @Mapping(source = "unit.id", target = "unitId"),
+        @Mapping(target = "invoiceNumber", expression = "java(mapInvoiceIds(lease.getInvoices()))")
+    })
+    LeaseDTO toDTO(Lease lease);
+
+    /**
+     * Helper method to map list of Invoice IDs.
+     */
+    default List<String> mapInvoiceIds(List<Invoice> invoices) {
+        if (invoices == null) {
+            return null;
+        }
+        return invoices.stream()
+                .map(Invoice::getInvoiceNumber)
+                .collect(Collectors.toList());
+    }
 }
