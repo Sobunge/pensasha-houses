85802358d35e93ece2e7a60d9367389e2271929e
Samuel Obunge <samuelobunge@gmail.com>
2025-08-08 21:09:46 +0300

    Updating app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
+import LoginPage from './pages/Auth/LoginPage/LoginPage';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path='/login' element={<LoginPage />} />
       </Routes>
     </Router>
   );
