6982cb7021bb84dec38b9e66e1800e7783941982
Samuel Obunge <samuelobunge@gmail.com>
2025-09-01 12:59:49 +0000

    Removing the unused import in the landingPage

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,4 @@
 import { Box } from '@mui/material';
-import Navbar from '../../components/Navbar';
 import Hero from '../../pages/LandingPage/Hero'
 import FeaturedListings from '../../pages/LandingPage/FeaturedListings'
 import HotDeals from '../../pages/LandingPage/HotDeals'
