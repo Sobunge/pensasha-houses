5edb0352f6fc8d397bf49f409fedff627eef6776
Sobunge <samuelobunge@gmail.com>
2025-12-29 20:55:24 +0300

    Adding document repository

---
+++ backend/src/main/java/com/pensasha/backend/modules/document/DocumentRepository.java
@@ -0,0 +1,7 @@
+package com.pensasha.backend.modules.document;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DocumentRepository extends JpaRepository<Document, Long> {
+    
+}
