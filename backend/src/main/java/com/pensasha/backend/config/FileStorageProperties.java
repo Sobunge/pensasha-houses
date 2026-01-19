49fd581da40dc545cd3c4c799f0a33643f686cef
Sobunge <samuelobunge@gmail.com>
2026-01-19 19:56:23 +0300

    added the file storage properties

---
+++ backend/src/main/java/com/pensasha/backend/config/FileStorageProperties.java
@@ -0,0 +1,12 @@
+package com.pensasha.backend.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "file")
+@Data
+public class FileStorageProperties {
+    private String uploadDir;
+}
