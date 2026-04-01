a1736a76dacb1fea096e0ed2eb3a0c5fa6ea59ee
Samuel Obunge <samuelobunge@gmail.com>
2026-04-01 13:37:13 +0300

    updating the permission class

--- backend/src/main/java/com/pensasha/backend/modules/user/Permission.java
+++ backend/src/main/java/com/pensasha/backend/modules/user/Permission.java
@@ -5,9 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "permissions")
+@Setter
+@Getter
 public class Permission {
     @Id @GeneratedValue
     private Long id;
