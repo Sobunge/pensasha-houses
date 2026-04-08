559dd50e3c7aba018fafea0cb998a4233fb1c162
Sobunge <samuelobunge@gmail.com>
2026-04-08 23:33:33 +0300

    Adding a passwordResetToken

---
+++ backend/src/main/java/com/pensasha/backend/modules/user/PasswordResetToken.java
@@ -0,0 +1,37 @@
+package com.pensasha.backend.modules.user;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+public class PasswordResetToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    private LocalDateTime expiryDate;
+
+    public PasswordResetToken(String token, User user) {
+        this.token = token;
+        this.user = user;
+        this.expiryDate = LocalDateTime.now().plusMinutes(30); // Valid for 30 mins
+    }
+}
